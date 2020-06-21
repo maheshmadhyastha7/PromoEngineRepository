d51a9b63c954ad1e1413a30111cc2a1fa0bdeac9
princemaheshec@gmail.com <mahesh.madhyastha@siemensgamesa.com>
2020-06-21 10:50:38 +0530

    Added the different Promo code handlers

--- PromoEngineRepository/PromoEngineClient.cs
+++ PromoEngineRepository/PromoEngineClient.cs
@@ -7,15 +7,18 @@ namespace PromoEngineRepository
 
         public int ApplyPromo(string selection, List<string> orders)
         {
-            PromoEngineHandler promotionHandler;
+            PromoEngineHandler promoHandler;
             switch (selection)
             {
                 case "3 of A's for 130":
-                    break;
+                    promoHandler = new PromoEngineHandler(new PromotionOnA());
+                    return promoHandler.ApplyPromotionCode(orders);
                 case "2 of B's for 45":
-                    break;
+                    promoHandler = new PromoEngineHandler(new PromotionOnB());
+                    return promoHandler.ApplyPromotionCode(orders);
                 case "C & D for 30":
-                    break;
+                    promoHandler = new PromoEngineHandler(new PromotionOnCAndD());
+                    return promoHandler.ApplyPromotionCode(orders);
                 default:
                     break;
             }
