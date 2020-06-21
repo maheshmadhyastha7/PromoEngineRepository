db600903f170ce6169db798c5bbfb9cf5be5a522
princemaheshec@gmail.com <mahesh.madhyastha@siemensgamesa.com>
2020-06-21 10:45:21 +0530

    Adding the Strategy for handling the promo codes. Helps in adding future promo codes

--- PromoEngineRepository/PromoEngineHandler.cs
+++ PromoEngineRepository/PromoEngineHandler.cs
@@ -8,5 +8,12 @@ namespace PromoEngineRepository
 {
     public class PromoEngineHandler
     {
+        private IPromoStrategy _promoStrategy;
+        public PromoEngineHandler(IPromoStrategy promoStrategy)
+        {
+            _promoStrategy = promoStrategy;
+        }
+        public int ApplyPromotionCode(List<string> orders)
+        => _promoStrategy.ApplyPromotionCode(orders);
     }
 }
