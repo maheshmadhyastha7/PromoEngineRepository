fd351622925203589d9ebc2343cb82633a80de9f
princemaheshec@gmail.com <mahesh.madhyastha@siemensgamesa.com>
2020-06-21 10:48:15 +0530

    Added class for handling various promo codes using Strategy pattern

---
+++ PromoEngineRepository/PromotionOnA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoEngineRepository
+{
+    public class PromotionOnA : IPromoStrategy
+    {
+        public int ApplyPromotionCode(List<string> orders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
