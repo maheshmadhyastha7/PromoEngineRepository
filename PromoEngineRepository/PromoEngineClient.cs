8799e0fe62b566e03b4c9b6dc3d1bdae789f3161
princemaheshec@gmail.com <mahesh.madhyastha@siemensgamesa.com>
2020-06-21 10:43:05 +0530

    Added the cases for choosing any one promo code as per requirement

--- PromoEngineRepository/PromoEngineClient.cs
+++ PromoEngineRepository/PromoEngineClient.cs
@@ -7,6 +7,18 @@ namespace PromoEngineRepository
 
         public int ApplyPromo(string selection, List<string> orders)
         {
+            PromoEngineHandler promotionHandler;
+            switch (selection)
+            {
+                case "3 of A's for 130":
+                    break;
+                case "2 of B's for 45":
+                    break;
+                case "C & D for 30":
+                    break;
+                default:
+                    break;
+            }
 
             return 0;
         }
